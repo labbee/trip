526a00adcabe3b0e1ae8adbeacc26183045bb960
JetLu <i@lufei.so>
2018-02-01 19:08:10 +0800

    :mushroom:

--- src/game/index.js
+++ src/game/index.js
@@ -3,13 +3,14 @@ import setLoader from './loader'
 import setLayer from './layer'
 import setTram from './tram'
 import setHuman from './human'
-export {key} from './monitor'
+import setKey from './monitor'
 
 
 export const load = setLoader(core)
 export const Layer = setLayer(core)
 export const Tram = setTram(core)
 export const Human = setHuman(core)
+export const key = setKey(core)
 export {core}
 
 
--- src/game/monitor.js
+++ src/game/monitor.js
@@ -1,4 +1,31 @@
-export const key = {}
+const key = {}
+
+let core
+
+export default function(_core) {
+    core = _core
+    return key
+}
+
+window.addEventListener('touchstart', ev => {
+    const point = ev.touches[0]
+
+    let x, w
+
+    if (core.setting.rotation === 90) {
+        x = point.clientY
+        w = window.innerHeight
+    } else {
+        x = point.clientX
+        w = window.innerWidth
+    }
+
+    x <= w / 2 ? key.left = true : key.right = true
+})
+
+window.addEventListener('touchend', ev => {
+    key.left = key.right = false
+})
 
 window.addEventListener('keydown', ev => {
     switch (ev.keyCode) {
